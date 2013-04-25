5c5edac82b9b5e7d1627613bdb9f113e0536c74e
samanthavandapuye <samantha.vandapuye@gmail.com>
2013-04-25 15:17:11 -0400

    header code

--- wp-content/themes/mayer-law/header.php
+++ wp-content/themes/mayer-law/header.php
@@ -41,6 +41,23 @@
 
 </script>
 
+<!-- Google Code for Remarketing tag -->
+<!-- Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. For instructions on adding this tag and more information on the above requirements, read the setup guide: google.com/ads/remarketingsetup -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 953297343;
+var google_conversion_label = "dYRcCMHlwgUQv9PIxgM";
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="www.googleadservices.com/pagead/conversion.js">;</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/953297343/?value=0&amp;label=dYRcCMHlwgUQv9PIxgM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
 <title><?php
 	/*
 	 * Print the <title> tag based on what is being viewed.
